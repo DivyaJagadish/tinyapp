93a88a2c5ba5f7984d4e8928cc6ea9c08bd19f1e
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-16 03:28:13 +0000

    function generate random string in global scope

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;//default port
+const bodyParser = require("body-parser"); //
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -18,7 +20,10 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase [req.params.shortURL]};
   res.render("url_show", templateVars);
 });
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -32,3 +37,8 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString () {
+  return (Math.random().toString(36).substring(2,8));
+};
+
