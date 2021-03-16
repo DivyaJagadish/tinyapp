eca2106aec97501f4c5dd01059605976b36e9c21
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-16 00:48:44 +0000

    addition of urls_index.ejs

--- express_server.js
+++ express_server.js
@@ -8,7 +8,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.get("/urls", (req, res) => {
+  res.render("urls_index", urlDatabase);
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
