dc7573d05ed1810ccc51f90955de31b6307b343d
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-17 00:45:37 +0000

    required cookie parser

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;//default port
-const bodyParser = require("body-parser"); //
+const bodyParser = require("body-parser"); // miidleware
+const cookieParser =require("cookie-parser");//middleware cookie parser
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 let urlDatabase = {
