573961d809181f753db41d0050a1602029e72751
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-17 02:22:56 +0000

    /logout route implemented

--- express_server.js
+++ express_server.js
@@ -53,6 +53,11 @@ app.post("/login",(req,res)=>{// login route using res.cookies
   res.cookie("username",req.body.username);
   res.redirect("/urls");
 });
+
+app.post("/logout",(req,res)=>{
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
 // });
