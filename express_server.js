14ff3ccbf1d55fc4102dcc1682e99eb7be51e7c1
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-18 19:28:26 +0000

    Formatting update

--- express_server.js
+++ express_server.js
@@ -21,8 +21,8 @@ let users = { //users database
 };
 
 let urlDatabase = {
-  "b2xVn2": {longURL:"http://www.lighthouselabs.ca", userId : "abxcdf"},
-  "9sm5xK": {longURL:"http://www.google.com" , userId: "abxcdf"}
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userId: "abxcdf" },
+  "9sm5xK": { longURL: "http://www.google.com", userId: "abxcdf" }
 };
 const generateRandomString = function () { // generates random string of 6 length.
   return (Math.random().toString(36).substring(2, 8));
@@ -36,42 +36,43 @@ const userAlreadyRegistered = function (newuseremail) {//check userAlreadyregist
   }
   return true;
 }
-const urlsForUser = function(id){// Filters the user generated URLs on the basis of id and then sends them back
-  const userurls ={};
+const urlsForUser = function (id) {// Filters the user generated URLs on the basis of id and then sends them back
+  const userurls = {};
   for (const url in urlDatabase) {
-  if(urlDatabase[url]["userId"]=== id) {
-  userurls[url] =urlDatabase[url];
-  }}
+    if (urlDatabase[url]["userId"] === id) {
+      userurls[url] = urlDatabase[url];
+    }
+  }
   return userurls;
 
 }
 
 app.get("/urls", (req, res) => {//urls page displays msg to if not logged in otherwise displays the urls ;
- const userurls= urlsForUser(req.cookies.userid);
+  const userurls = urlsForUser(req.cookies.userid);
   res.render("urls_index", { userurls: userurls, user: users[req.cookies.userid] });//sends only user generated urls.
 });
 app.get("/urls/new", (req, res) => { // generates a form for newURL+*-
   const userId = req.cookies.userid;// checks whether user is logged in or not Only registered users can create url 
-  if(userId !== undefined){
-  res.render("urls_new", { user: users[req.cookies.userid] });
-  } else 
-  res.redirect("/login");
+  if (userId !== undefined) {
+    res.render("urls_new", { user: users[req.cookies.userid] });
+  } else
+    res.redirect("/login");
 });
 
 app.get("/urls/:shortURL", (req, res) => {// shows the page what the shortURL corresponds to shorturl  if user logged in otherwise prompt to login or messages that they don't own the url.
-  const userurls= urlsForUser(req.cookies.userid)
- 
-  if(userurls[`${req.params.shortURL}`]) {
-  const templateVars = { shortURL: req.params.shortURL, longURL: userurls[`${req.params.shortURL}`]["longURL"], user: users[req.cookies.userid] };
+  const userurls = urlsForUser(req.cookies.userid)
+
+  if (userurls[`${req.params.shortURL}`]) {
+    const templateVars = { shortURL: req.params.shortURL, longURL: userurls[`${req.params.shortURL}`]["longURL"], user: users[req.cookies.userid] };
     res.render("url_show", templateVars);
-  }else {
+  } else {
     const templateVars = { shortURL: req.params.shortURL, longURL: undefined, user: users[req.cookies.userid] };
     res.render("url_show", templateVars);
   }
 });
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  let newShortURl= {longURL :req.body.longURL, userId : req.cookies.userid};
+  let newShortURl = { longURL: req.body.longURL, userId: req.cookies.userid };
   urlDatabase[shortURL] = newShortURl;
   res.redirect(`urls/${shortURL}`);         //Redirect the page to the shortURL
 });
@@ -82,18 +83,18 @@ app.get("/u/:shortURL", (req, res) => { // Redirect the shortURL to actual web p
 
 app.post("/urls/:shortURL/delete", (req, res) => {// delete a particular shortURL from the url Database only users who created can do that 
   const userurls = urlsForUser(req.cookies.userid);
-  if(userurls[`${req.params.shortURL}`]) {// can delete only urls created by user
-  const id = req.params.shortURL;
-  delete urlDatabase[id];
+  if (userurls[`${req.params.shortURL}`]) {// can delete only urls created by user
+    const id = req.params.shortURL;
+    delete urlDatabase[id];
   }
   res.redirect("/urls");
 
 })
 app.post("/urls/:shortURL/edit", (req, res) => { //edit the long url and redirect to Myurl Page.
   const userurls = urlsForUser(req.cookies.userid);
-  if(userurls[`${req.params.shortURL}`]) {//can edit only url created by user
-  const shortURL = req.params.shortURL;
-  urlDatabase[shortURL]["longURL"] = req.body.longURL;
+  if (userurls[`${req.params.shortURL}`]) {//can edit only url created by user
+    const shortURL = req.params.shortURL;
+    urlDatabase[shortURL]["longURL"] = req.body.longURL;
   }
   res.redirect("/urls");
 
